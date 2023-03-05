b8125e9058156de617e03f3b4f987357b71e8f0c
Kevin Frejdh <keffler97@live.se>
2023-03-05 12:42:48 +0100

    Script now runs the command directly

---
+++ scripts/run-bump-version-command.js
@@ -0,0 +1,40 @@
+// https://github.com/actions/github-script
+
+module.exports = async ({github, context, core}) => {
+    const {
+        projectFramework,
+        version,
+    } = process.env;
+
+    let commandOutput = '';
+    let commandError = '';
+    const options = {
+        listeners: {
+            stdout: (data) => {
+                commandOutput += data.toString();
+            },
+            stderr: (data) => {
+                commandError += data.toString();
+            }
+        }
+    };
+    const exec = require('@actions/exec');
+
+    switch (projectFramework.toLowerCase()) {
+        case 'maven':
+            exec.exec("mvn", ["-B", "versions:set", `-DnewVersion=${version}`, "-DprocessAllModules", "-DgenerateBackupPoms=false"], options);
+            break;
+    }
+
+    if (commandError) {
+        core.notice(commandOutput);
+        core.setFailed(commandError);
+    }
+    else if (commandOutput) {
+        core.info(`Resolved command for [${projectFramework}] and executed command successfully`);
+    }
+    else {
+        core.setFailed(`Project framework [${projectFramework}] not supported`);
+    }
+
+}
